4d7fc0a171dec58e22db677d2359633c288d403c
RamziK23 <ramzesdavletbaev23@gmail.com>
2020-10-22 11:40:44 +0500

    Revert "V1.3"
    This reverts commit 552e872276210c7a4d9c0992421d6f41b749d953.

--- Lab2_oop/Lab2_oop.cpp
+++ Lab2_oop/Lab2_oop.cpp
@@ -1,6 +1,5 @@
 ﻿#include <iostream>
 #include <conio.h>
-#include <vector>
 
 class Point{
 public:
@@ -26,28 +25,6 @@ public:
 		}
 };
 
-class Line{
-public:
-	int x1, y1, x2, y2;
-	Line(int x1, int y1, int x2, int y2) {
-		Point p1(x1, y1);
-		Point p2(x2, y2);
-		printf("Line(int x1, int y1, int x2, int y2)\n");
-	}
-	Line(const Line &l) {
-		printf("Line(const Line &p)\n");
-		x1 = l.x1;
-		y1 = l.y1;
-		x2 = l.x2;
-		y2 = l.y2;
-	}
-	~Line() {
-		/*printf("%d,%d,%d,%d\n", x1, y1, x2, y2);*/
-		printf("~Line\n");
-	}
-
-};
-
 int main()
 {
 	{
@@ -56,11 +33,6 @@ int main()
 		Point p3(p2);
 	}
 
-	{
-		Line l1(5, 10, 15, 20);
-		Line l2(l1);
-	}
-	
 	_getch();
 	return 0;
 };
